57ab60625535a07024458c95cc1a8e7f780095d6
bryant.gossett@gmail.com <5419632+Bgosse1@users.noreply.github.com>
2018-10-25 21:39:32 -0400

    WIP

---
+++ assets/javascript/app.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+    var questions = {
+        question_one:{
+            question:"What was the first full length CGI movie?",
+            choices:[],
+            answer:wip,
+
+        },
+        question_two:{
+
+        },
+        question_three:{
+            
+        },
+        question_four:{
+
+        },
+        question_five:{
+
+        },
+        question_six:{
+
+        },
+        question_seven:{
+
+        },
+        question_eight:{
+
+        }
+    };
+    var triviaGame = {
+
+    }
+
+
+});
